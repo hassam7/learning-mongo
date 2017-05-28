3f20ffd723f99194a522acc7fdcd9e45522a52f0
Hassam7 <hassam.ali7@hotmail.com>
2017-05-28 21:17:09 +0500

    remove existing sub document

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -19,16 +19,47 @@ describe("Subdocuments", () => {
         });
     });
     it("can add subdocuments to an existing record", (done) => {
-        var joe = new User({name:"Joe"});
-        joe.save().then(()=>{
-            return User.findOne({name:"Joe"})
-        }).then((user)=>{
-            user.posts.push({title:"New Post"});
+        var joe = new User({
+            name: "Joe"
+        });
+        joe.save().then(() => {
+            return User.findOne({
+                name: "Joe"
+            })
+        }).then((user) => {
+            user.posts.push({
+                title: "New Post"
+            });
             return user.save();
-        }).then(()=>{
-            return User.findOne({name:"Joe"});
-        }).then((user)=>{
+        }).then(() => {
+            return User.findOne({
+                name: "Joe"
+            });
+        }).then((user) => {
             assert(user.posts[0].title === "New Post");
+            done();
+        });
+    });
+    it("can remove an existing subdocuments ", (done) => {
+        const joe = new User({
+            name: "Joe",
+            posts: [{
+                title: "New Post One"
+            }]
+        });
+        joe.save().then(() => {
+            return User.findOne({
+                name: "Joe",
+            });
+        }).then((user) => {
+            user.posts[0].remove();
+            return user.save();
+        }).then(() => {
+            return User.findOne({
+                name: "Joe"
+            });
+        }).then((user) => {
+            assert(user.posts.length === 0);
             done();
         });
     });
