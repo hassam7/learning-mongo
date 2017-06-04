a25133b992a57831cf13c278aa1b96b414d7f1f8
Hassam7 <hassam.ali7@hotmail.com>
2017-06-04 16:06:14 +0500

    mongoose remove hook

--- src/user.js
+++ src/user.js
@@ -31,6 +31,21 @@ const UserSchema = new Schema({
 UserSchema.virtual("postCount").get(function () {
     return this.posts.length; //refers to current instance of Model
 }); //a Virtual field named postCount
+UserSchema.pre("remove",function(next){
+    //this === refers to current instance of user
+    //if we used fat arrow function the context of this will be that of parent
+    
+    /*
+        To remove blogPost we can require blogPost and then call remove method on it.
+        But it can cause cyclic dependencies. So it's better to use mongoose.
+    */
+    const BlogPost = mongoose.model('blogPost');
+    BlogPost.remove( { id : { $in : this.blogPost } } )
+     .then(()=>next());
+    //go through all the blogPost and remove if the id field matches the id field of
+    //this.blogPost
+});
+
 const User = mongoose.model('user', UserSchema);
 //^--> What collection is called inside MongoDB
 // if it does not exists mongodb creates 
