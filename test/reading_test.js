350078cd54cb2c289b06aa8466d97ffe0c56316e
Hassam7 <hassam.ali7@hotmail.com>
2017-06-04 16:31:27 +0500

    add skip, limit and sort operators

--- test/reading_test.js
+++ test/reading_test.js
@@ -1,14 +1,20 @@
 const assert = require('assert');
 const User = require('../src/user');
 describe("Reading users out of database", () => {
-    let tom;
+    let tom,maria,alex,zach;
+
     beforeEach((done) => {
+        alex = new User({name:"Alex"});
+        maria = new User({name:"Maria"});
+        zach = new User({name:"Zach"});
         tom = new User({
             name: 'Tom'
         });
-        tom.save().then(() => {
-            done();
-        });
+
+        Promise.all([alex.save(),maria.save(),zach.save(),tom.save()])
+               .then(()=>{
+                    done();
+               });
     });
     it("find all users with name of tom", (done) => {
         User.find({
@@ -24,6 +30,19 @@ describe("Reading users out of database", () => {
         }).then((user) => {
              assert(user.name==='Tom');
              done();
+        });
+    });
+
+    it.only(" can skip and limit the result set",(done)=>{
+        User.find({})
+        .sort({name:1}).
+        skip(1).
+        limit(2)
+        .then((users)=>{
+            assert(users.length === 2);
+            assert(users[0].name === 'Maria');
+            assert(users[1].name === 'Tom');
+            done();
         });
     });
 });
